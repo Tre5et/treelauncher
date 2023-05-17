5fbb78b81f03c3258fd60debb11e78d6bc665342
hanneshekl@gmail.com <hanneshekl@gmail.com>
2023-05-17 17:08:21 +0200

    Cleaned up git repository

--- src/main/java/net/treset/minecraftlauncher/LauncherApplication.java
+++ src/main/java/net/treset/minecraftlauncher/LauncherApplication.java
@@ -2,7 +2,6 @@ package net.treset.minecraftlauncher;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import net.treset.minecraftlauncher.auth.UserAuth;
 import net.treset.minecraftlauncher.config.Config;
 import net.treset.minecraftlauncher.resources.localization.StringLocalizer;
@@ -71,7 +70,6 @@ public class LauncherApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         System.setProperty("prism.lcdtext", "false");
-        primaryStage.initStyle(StageStyle.UNIFIED);
 
         LoginController.showOnStage(primaryStage);
     }
