4a9c15787eafe930ef9d79fbb14d660c597d2285
TreSet <79938957+Tre5et@users.noreply.github.com>
2023-09-11 15:59:18 +0200

    Moved files to client director and added server

---
+++ server/target/debug/deps/file_sync_server.d
@@ -0,0 +1,5 @@
+C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\target\debug\deps\file_sync_server.exe: src\main.rs
+
+C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\target\debug\deps\file_sync_server.d: src\main.rs
+
+src\main.rs:
---
+++ server/target/debug/file-sync-server.d
@@ -0,0 +1,1 @@
+C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\target\debug\file-sync-server.exe: C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\src\main.rs
