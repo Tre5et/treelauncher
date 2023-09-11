664ffa7780171f3f8cb7557a32ff1851f482a5f1
TreSet <79938957+Tre5et@users.noreply.github.com>
2023-09-11 16:09:08 +0200

    Added server gitignore

--- server/target/debug/deps/file_sync_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\target\debug\deps\file_sync_server.exe: src\main.rs
-
-C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\target\debug\deps\file_sync_server.d: src\main.rs
-
-src\main.rs:
--- server/target/debug/file-sync-server.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\target\debug\file-sync-server.exe: C:\Users\Hannes-Surface\_Data\Coding\treelauncher\server\src\main.rs
