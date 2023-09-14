ac4e54986a4ca7ed2154ec87e31f61cbfbcb13d5
TreSet <hanneshekl@gmail.com>
2023-09-14 22:16:06 +0200

    Implemented file post and authentication

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,33 @@
+use std::fs;
+use std::path::Path;
 use actix_web::{App, HttpServer};
+use once_cell::sync::OnceCell;
+
 mod service;
 mod component_type;
 
+static AUTH_KEY: OnceCell<String> = OnceCell::new();
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let path = Path::new("key.auth");
+    if !path.is_file() {
+        return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "Auth key not found!"));
+    }
+    let key = fs::read_to_string(path).expect("Unable to read auth key!");
+    AUTH_KEY.set(key).expect("Unable to set auth key!");
+
     HttpServer::new(|| {
         App::new()
             .service(service::get_file)
+            .service(service::post_file)
             .service(service::complete)
     })
     .bind("0.0.0.0:9090")?
     .run()
     .await
+}
+
+pub fn get_auth_key() -> String {
+    return AUTH_KEY.get().expect("Auth key not set!").clone();
 }
--- server/src/service.rs
+++ server/src/service.rs
@@ -1,13 +1,14 @@
 use std::fs;
 use std::path::Path;
-use actix_web::{get, HttpResponse, HttpRequest};
+use actix_web::{get, HttpResponse, HttpRequest, post};
 use actix_web::http::header::ContentType;
 use base64::Engine;
 use serde::{Serialize, Deserialize};
 use crate::component_type::ComponentType;
+use crate::get_auth_key;
 
 #[derive(Serialize, Deserialize)]
-pub struct FileResponse {
+pub struct FileData {
     path: String,
     content: String
 }
@@ -39,13 +40,47 @@ pub async fn get_file(req: HttpRequest) -> HttpResponse {
     let content = base64::engine::general_purpose::STANDARD.encode(fs::read(file).expect("Unable to read file!"));
     return HttpResponse::Ok()
         .content_type(ContentType::json())
-        .json(FileResponse {
+        .json(FileData {
             path: file_path,
             content
         })
 }
 
-pub fn check_prerequisites<'a>(req: HttpRequest) -> Result<String, HttpResponse> {
+#[post("file/{component_type}/{id}")]
+pub async fn post_file(req: HttpRequest, body: String) -> HttpResponse {
+    let result = check_prerequisites(req);
+    if result.is_err() {
+        return result.err().unwrap();
+    }
+    let base_path = result.unwrap();
+    let file_response = serde_json::from_str(body.as_str());
+    if file_response.is_err() {
+        return HttpResponse::BadRequest()
+            .body(format!("Invalid file response! Error: {}", file_response.err().unwrap()));
+    }
+    let file_response: FileData = file_response.unwrap();
+    let path = format!("{b}/{p}", b = base_path, p = file_response.path);
+    let file: &Path = Path::new(path.as_str());
+    if !file.is_file() {
+        let prefix = file.parent().unwrap();
+        fs::create_dir_all(prefix).unwrap();
+        fs::File::create(file).unwrap();
+    }
+    let content = base64::engine::general_purpose::STANDARD.decode(file_response.content.as_bytes()).unwrap();
+    let result = fs::write(file, content);
+    if result.is_err() {
+        return HttpResponse::InternalServerError()
+            .body(format!("Unable to write file! Error: {}", result.err().unwrap()));
+    }
+    return HttpResponse::Ok().finish();
+}
+
+pub fn check_prerequisites(req: HttpRequest) -> Result<String, HttpResponse> {
+    let key_header = req.headers().get("auth-key");
+    if key_header.is_none() || key_header.unwrap() != &get_auth_key() {
+        return Err(HttpResponse::Forbidden().finish());
+    }
+
     let component_type = req.match_info().query("component_type").parse::<ComponentType>();
     if component_type.is_err() {
         return Err(HttpResponse::BadRequest()
