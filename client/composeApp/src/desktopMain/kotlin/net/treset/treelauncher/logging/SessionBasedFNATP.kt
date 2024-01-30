cb78f60ab97ac71ca10f0e85c30e70982d0003f6
TreSet <hanneshekl@gmail.com>
2024-01-30 22:15:27 +0100

    Added Proper Logging

---
+++ client/composeApp/src/desktopMain/kotlin/net/treset/treelauncher/logging/SessionBasedFNATP.kt
@@ -0,0 +1,67 @@
+// Package changing strangeness needed to access tbrp.fileNamePattern correctly
+package ch.qos.logback.core.rolling
+
+import ch.qos.logback.core.joran.spi.NoAutoStart
+import ch.qos.logback.core.rolling.helper.CompressionMode
+import ch.qos.logback.core.rolling.helper.FileFilterUtil
+import java.io.File
+
+@NoAutoStart
+class SessionBasedFNATP<E> : TimeBasedFileNamingAndTriggeringPolicyBase<E>() {
+    private var currentPeriodsCounter = 0
+
+    override fun start() {
+        super.start()
+
+        setDateInCurrentPeriod(System.currentTimeMillis())
+
+        computePeriodCounter()
+
+        started = true
+    }
+
+    private fun computePeriodCounter() {
+        val regex = tbrp.fileNamePattern.toRegexForFixedDate(dateInCurrentPeriod)
+        val stemRegex = FileFilterUtil.afterLastSlash(regex)
+
+        val file = File(currentPeriodsFileNameWithoutCompressionSuffix)
+        val parentDir = file.parentFile
+
+        val matchingFileArray = FileFilterUtil.filesInFolderMatchingStemRegex(parentDir, stemRegex)
+
+        if (matchingFileArray == null || matchingFileArray.isEmpty()) {
+            currentPeriodsCounter = 0
+            return
+        }
+        currentPeriodsCounter = FileFilterUtil.findHighestCounter(matchingFileArray, stemRegex)
+
+        if (tbrp.parentsRawFileProperty != null || (tbrp.compressionMode != CompressionMode.NONE)) {
+            currentPeriodsCounter++
+        }
+    }
+
+    override fun isTriggeringEvent(activeFile: File?, event: E): Boolean {
+        if(ready) {
+            ready = false
+            elapsedPeriodsFileName = tbrp.fileNamePatternWithoutCompSuffix.convertMultipleArguments(
+                dateInCurrentPeriod,
+                currentPeriodsCounter
+            )
+            currentPeriodsCounter++
+            return true
+        }
+        return false
+    }
+
+    override fun getCurrentPeriodsFileNameWithoutCompressionSuffix(): String {
+        return tbrp.fileNamePatternWithoutCompSuffix.convertMultipleArguments(
+            dateInCurrentPeriod,
+            currentPeriodsCounter
+        )
+    }
+
+
+    companion object {
+        var ready = true
+    }
+}
---
+++ client/composeApp/src/desktopMain/kotlin/net/treset/treelauncher/logging/SessionBasedRollingPolicy.kt
@@ -0,0 +1,19 @@
+package net.treset.treelauncher.logging
+
+import ch.qos.logback.core.rolling.SessionBasedFNATP
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+
+class SessionBasedRollingPolicy<E> : TimeBasedRollingPolicy<E>() {
+
+    override fun start() {
+        val sessionBasedFNATP = SessionBasedFNATP<E>()
+
+        timeBasedFileNamingAndTriggeringPolicy = sessionBasedFNATP
+
+        super.start()
+    }
+
+    override fun toString(): String {
+        return "c.q.l.core.rolling.SessionBasedRollingPolicy@" + this.hashCode()
+    }
+}
