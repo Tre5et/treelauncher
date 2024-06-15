d4cfd94322a135c581a4be1e51f0b03f1c76b36c
TreSet <hanneshekl@gmail.com>
2024-06-15 16:35:06 +0200

    Fix: Fixed List View localization

--- client/composeApp/src/commonMain/kotlin/net/treset/treelauncher/util/ListViewUtil.kt
+++ client/composeApp/src/commonMain/kotlin/net/treset/treelauncher/util/ListViewUtil.kt
@@ -5,9 +5,9 @@ import net.treset.treelauncher.localization.strings
 enum class DetailsListDisplay(
     val displayName: () -> String
 ) {
-    FULL(strings().list.full),
-    COMPACT(strings().list.compact),
-    MINIMAL(strings().list.minimal);
+    FULL({ strings().list.full() }),
+    COMPACT({ strings().list.compact() }),
+    MINIMAL({ strings().list.minimal() });
 
     override fun toString(): String = displayName()
 }
